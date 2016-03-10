ab361476c92c237b4592d31515a3d618fe6788eb
= <=>
2016-03-10 17:14:39 +0530

    added program for control statement

--- types_variables_control_structures/control_structures.go
+++ types_variables_control_structures/control_structures.go
@@ -1,3 +1,6 @@
+
+
+
 package main
 
 import (
@@ -6,18 +9,68 @@ import (
 const start = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%v~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
 const end = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
 
-func for_statement() {
+// For loop example
+
+func for_loop(n int){
+  //
+  var pre int = 0
+  var pre1 int = 1
+  var feb_series int
+  fmt.Println("0\n1")
+  for i := 2; i < n; i++ {
+    feb_series = pre + pre1
+    fmt.Println(feb_series)
+    pre = pre1
+    pre1 = feb_series
+  }
+}
+
+
+func if_else_statement(n int) {
+  if (n < 2000){
+    fmt.Printf("The year %d is not exsist in 21st century\n",n)
+  }else if(n % 4 == 0) {
+    fmt.Printf("The year %d is a leap year\n",n)
+  }else {
+   fmt.Printf("The year %d is not a leap year\n",n)
+  }
 
 }
 
-func if_statement() {
-
-}
-
-func switch_statement() {
-
+func switch_statement(month int) {
+  switch month {
+    case 1: fmt.Println("Jan")
+    case 2: fmt.Println("Feb")
+    case 3: fmt.Println("March")
+    case 4: fmt.Println("Apr")
+    case 5: fmt.Println("May")
+    case 6: fmt.Println("June")
+    case 7: fmt.Println("July")
+    case 8: fmt.Println("Aug")
+    case 9: fmt.Println("Sep")
+    case 10: fmt.Println("Oct")
+    case 11: fmt.Println("Nov")
+    case 12: fmt.Println("Dec")
+    default: fmt.Println("The number Should with in 1 to 12")
+  }
 }
 
 func main() {
+  fmt.Printf(start,"Fibonacci Sequence using For Loop")
+  fmt.Println("Enter Last Number Of Series:")
+  var n int
+  fmt.Scanf("%d",&n)
+  for_loop(n)
+  fmt.Printf(end)
+  fmt.Printf(start,"Year is leap year or not")
+  fmt.Print("Enter the year :")
+  fmt.Scanf("%d",&n)
+  if_else_statement(n)
+  fmt.Printf(end)
+  fmt.Printf(start,"number to Month Name")
+  fmt.Print("Enter the Number :")
+  fmt.Scanf("%d",&n)
+  switch_statement(n)
+  fmt.Printf(end)
 
 }
--- types_variables_control_structures/work-out-problems.go
+++ types_variables_control_structures/work-out-problems.go
@@ -19,7 +19,7 @@ func largestBase10Value(val float64) float64 {
 func Base_10_to_Base_2(val int) string {
   v1 := (val / 2)
   v2 := (val - (v1*2))
-  stringValue += strconv.Itoa(v2)
+  stringValue += strconv.Itoa(v2) // Type casting int to string
   if (v1 > 0) { Base_10_to_Base_2(v1)}
   return reverseString(stringValue)
 }
@@ -30,17 +30,50 @@ func reverseString(s string) string {
   return string(chars)
 }
 
+func list_numbers_divided_by_two_numbers(x,y,z int){
+  if (x < 1 || y < 1){
+    fmt.Println("Enter number greater than 0")
+  }else if( x == y) {
+    fmt.Println("Both are same Numbers")
+  }else{
+    for i:=2; i<=z; i++ {
+      if (i % y == 0 && i % x == 0) { fmt.Println(i)}
+    }
+  }
+}
+
+func min(m,n int) int {
+ if m > n {
+  return n
+ }else{
+  return m
+ }
+}
+
+
+strconv.Itoa(v2) // Type casting int to string
 
 func main() {
   fmt.Print(start,"Bigest Number")
   fmt.Print("Enter the length of the digit:")
-  // var i float64
-  var j int
-  // fmt.Scanf("%g",&i)
-  // fmt.Println(largestBase10Value(i))
-  // fmt.Print(end)
-  // fmt.Print(start,"Base 10 --> Base 2")
+  var i float64
+  var j,k,n int
+  fmt.Scanf("%g",&i)
+  fmt.Println(largestBase10Value(i))
+  fmt.Print(end)
+  fmt.Print(start,"Base 10 --> Base 2")
   fmt.Print("Enter the :")
   fmt.Scanf("%d",&j)
   fmt.Println(Base_10_to_Base_2(j))
+  fmt.Print(end)
+  fmt.Print(start,"list Numbers commany divied by 2 numbers")
+  fmt.Print("Enter the Lenth of List :")
+  fmt.Scanf("%d",&n)
+  fmt.Print("Enter the 1st number :")
+  fmt.Scanf("%d",&k)
+  fmt.Print("Enter the 2nd number :")
+  fmt.Scanf("%d",&j)
+  list_numbers_divided_by_two_numbers(j,k,n)
+  fmt.Print(end)
+
 }
