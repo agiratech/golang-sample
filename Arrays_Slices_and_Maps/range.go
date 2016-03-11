462b1c2f4bfe0959712e36ed81808ac269389c92
= <=>
2016-03-11 11:37:30 +0530

    array-slices-mapes program added

--- Arrays_Slices_and_Maps/range.go
+++ Arrays_Slices_and_Maps/range.go
@@ -10,9 +10,9 @@ const start = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%v~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
 const end = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
 func main() {
   fmt.Printf(start,"Range")
-  array := make([]int,10,10) {1,2,3,4,5,6,7,8,9,10}
+  array := []int {1,2,3,4,5,6,7,8,9,10}
   hashTable := map[int]string { 1:"arun",2:"kumar"}
-  stringVal = "Arun"
+  stringVal := "Arun"
   fmt.Printf("The defined Map Is ~~~~~~~~~~~~~~~~~~~~> %v\n",hashTable)
   fmt.Printf("The defined Array Is ~~~~~~~~~~~~~~~~~~~~> %v\n",array)
   fmt.Printf("The defined String Is ~~~~~~~~~~~~~~~~~~~~> %v\n",stringVal)
@@ -23,28 +23,28 @@ func main() {
 }
 
 func map_with_range(m map[int]string) {
-  fmt.printf(start,"Map part executing...........")
-  _,m := range m {
-    fmt.printf("The value of the _ ---> %v\n",_)
-    fmt.printf("The value of the m ---> %v\n",m)
+  fmt.Printf(start,"Map part executing...........")
+  for a,m := range m {
+    fmt.Printf("The value of the _ ---> %v\n",a)
+    fmt.Printf("The value of the m ---> %v\n",m)
   }
   fmt.Printf(end)
 }
 
 func array_with_range(array []int) {
-  fmt.printf(start,"Array part executing...........")
-  _,num := range array {
-    fmt.printf("The value of the _ ---> %v\n",_)
-    fmt.printf("The value of the num ---> %v\n",num)
+  fmt.Printf(start,"Array part executing...........")
+  for a,num := range array {
+    fmt.Printf("The value of the _ ---> %v\n",a)
+    fmt.Printf("The value of the num ---> %v\n",num)
   }
   fmt.Printf(end)
 }
 
 func string_with_range(s string) {
-  fmt.printf(start,"String part executing...........")
-  _,str := range s {
-    fmt.printf("The value of the _ ---> %v\n",_)
-    fmt.printf("The value of the s ---> %v\n",s)
+  fmt.Printf(start,"String part executing...........")
+  for a,str := range s {
+    fmt.Printf("The value of the _ ---> %v\n",a)
+    fmt.Printf("The value of the s ---> %v\n",str)
   }
   fmt.Printf(end)
 }
