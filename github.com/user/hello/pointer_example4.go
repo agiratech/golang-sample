0b22cdd4dd76a37c700f24d5ecd063b29c2b4e45
Bharani <bharani@agiratech.com>
2016-03-11 12:16:58 +0530

    Go lang basics example and exercise file added

---
+++ github.com/user/hello/pointer_example4.go
@@ -0,0 +1,38 @@
+// Sample program to show varaibles stay on or escape from the stack.
+package main
+
+import "fmt"
+
+// main is the entry point for the application.
+func main() {
+	stayOnStack()
+	escapeToHeap()
+}
+
+// stayOnStack shows how the variable does not escape.
+func stayOnStack() {
+	// Declare a variable of type integer.
+	var x int
+
+	// Display the address of the variable.
+	println("Stack Addr:", &x)
+}
+
+// escapeToHeap shows how the variable does escape.
+func escapeToHeap() {
+	// Declare a variable of type integer.
+	var x int
+
+	// Display the address of the variable.
+	fmt.Println("Heap Addr:", &x)
+}
+
+/*
+./example1.go:20: can inline stayOnStack
+./example1.go:15: inlining call to stayOnStack
+./example1.go:31: moved to heap: x
+./example1.go:34: &x escapes to heap
+./example1.go:34: escapeToHeap ... argument does not escape
+./example1.go:15: stayOnStack &x does not escape
+./example1.go:25: stayOnStack &x does not escape
+*/
